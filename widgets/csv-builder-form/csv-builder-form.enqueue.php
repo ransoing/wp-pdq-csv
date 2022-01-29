3c123cab3f600d3d6182d32c870fc6bcd8441168
Ransom Christofferson <ransom.christofferson@lasp.colorado.edu>
2022-01-29 11:45:47 -0700

    use local version of moment

--- widgets/csv-builder-form/csv-builder-form.enqueue.php
+++ widgets/csv-builder-form/csv-builder-form.enqueue.php
@@ -18,8 +18,7 @@ require_once( __DIR__ . '/../../widgets/export-settings-dialog/export-settings-d
 <?php
 
 // add the js
-wp_enqueue_script( 'momentjs', 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js' );
-wp_enqueue_script( 'pdqcsv-csv-builder-form', plugin_dir_url(__FILE__) . 'csv-builder-form.js', ['pdqcsv-multi-fields', 'pdqcsv-multi-filters', 'pdqcsv-export-settings-dialog', 'jquery-ui-button', 'jquery-ui-dialog', 'momentjs'], PDQCSV_VERSION );
+wp_enqueue_script( 'pdqcsv-csv-builder-form', plugin_dir_url(__FILE__) . 'csv-builder-form.js', ['pdqcsv-multi-fields', 'pdqcsv-multi-filters', 'pdqcsv-export-settings-dialog', 'jquery-ui-button', 'jquery-ui-dialog', 'moment'], PDQCSV_VERSION );
 
 // add the css
 wp_enqueue_style( 'pdqcsv-spinner', plugin_dir_url(__FILE__) . '../spinner.css', [], PDQCSV_VERSION );
